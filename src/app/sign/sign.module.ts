4ad5398908febf507b0ad1c3972585b5c558b6eb
Andrés Alberca <andres.al314@hotmail.com>
2024-08-05 15:03:47 -0500

    add component in declarations of the module file and import FormsModule

--- src/app/sign/sign.module.ts
+++ src/app/sign/sign.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SignRoutingModule } from './sign-routing.module';
-
+import { SignInComponent } from './sign-in/sign-in.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignInComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     SignRoutingModule
   ]
 })
