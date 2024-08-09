d11626b17bf75b5afdbfcdcc1ab79eb29a1e4fe4
Andrés Alberca <andres.al314@hotmail.com>
2024-08-09 13:58:44 -0500

    add margin bottom to card for info component

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  API_URI = "http://localhost:3000/api"
+  API_URI = "https://elbuhoenseña.com/api"
 
   constructor(private http: HttpClient, private router: Router) { }
 
